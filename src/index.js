f10c2ca7e9491b2458a2f47703aef7767016986a
Reshika Samala <samala.reshika@gmail.com>
2020-03-09 00:27:08 -0700

    updated rest api url

--- src/index.js
+++ src/index.js
@@ -8,6 +8,22 @@ import Home from './Home';
 import 'bootstrap/dist/css/bootstrap.css'
 
 import * as serviceWorker from './serviceWorker';
+exports.handler = function(event, context, callback) {
+
+    callback(null, {
+        statusCode: '200',
+        body: "Hello world",
+        headers: {
+            // This is ALSO required for CORS to work. When browsers issue cross origin requests, they make a 
+            // preflight request (HTTP Options) which is responded automatically based on SAM configuration. 
+            // But the actual HTTP request (GET/POST etc) also needs to contain the AllowOrigin header. 
+            // 
+            // NOTE: This value is *not* double quoted: ie. "'www.example.com'" is wrong
+            "Access-Control-Allow-Origin": "http://reviewrestapi.us-west-2.elasticbeanstalk.com"
+        }
+    });
+
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
